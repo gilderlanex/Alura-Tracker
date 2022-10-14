928c3c18b20e045da91d6b05130bbc14f3f8c773
DOM\gilderlane.silva <gilderlane.silva>
2022-10-14 00:01:25 -0400

    adicionando nova rota e criação de novo componente

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,12 +1,20 @@
 import { RouteRecordRaw, createWebHashHistory, createRouter } from "vue-router";
 import Tarefas from '../views/Tarefas.vue';
+import Projetos from '../views/Projetos.vue'
 
 
-const rotas: RouteRecordRaw[] = [{
+const rotas: RouteRecordRaw[] = [
+    {
     path:'/', 
     name: 'Tarefas',
     component: Tarefas
-}];
+    },
+    {
+        path:'/projetos', 
+        name: 'Projetos',
+        component: Projetos
+    }
+];
 
 const roteador = createRouter({
     history: createWebHashHistory(),
