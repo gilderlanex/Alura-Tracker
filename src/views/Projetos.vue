3696bdd381fcc29f404af01f191b6abc8c654fcf
DOM\gilderlane.silva <gilderlane.silva>
2022-10-14 09:43:31 -0400

    views e rotas de tarefas e projetos

---
+++ src/views/Projetos.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1> Olá mundo</h1>
+
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Projetos'
+});
+</script>
